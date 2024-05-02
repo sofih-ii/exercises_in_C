91e7aa050461fb5d611c6cc05dfb22127b3f97d5
sofih-ii <sofiasaramoratrujillo@gmail.com>
2024-05-02 14:08:46 -0500

    Show the spare numbers from 1 to 100.

---
+++ exercise_4.c
@@ -0,0 +1,13 @@
+
+//show the spare numbers from 1 to 100.
+
+#include <stdio.h>
+int main (void){
+  int x, x1;
+  for(x = 1; x <= 100; x++){
+    if(x % 2 == 0){
+      printf("%d\n",x);
+    }
+  }
+  return 0;
+}

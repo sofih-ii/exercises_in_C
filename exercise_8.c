8f8b3c66b3a025fe72e0469d15c5bc4b0b48557c
sofih-ii <sofiasaramoratrujillo@gmail.com>
2024-05-03 12:04:08 -0500

    Shows the sum of the even numbers from 1 to 100, and how many it has

---
+++ exercise_8.c
@@ -0,0 +1,18 @@
+
+//Shows the sum of the even numbers from 1 to 100, and how many it has
+/*
+#include <stdio.h>
+int main(void){
+  int x, sum, cont;
+  sum = 0;
+  cont = 0;
+  for(x=1; x<=100; x++){
+    if(x%2!=0){
+      sum = sum+x;
+      cont++;
+    }
+  }
+  printf("%d\n", sum);
+  printf("la cantidad de numeros impares es : %d", cont);
+  return 0;
+}*/

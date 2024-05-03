3d20fea89b755fee6dc2092c9e539e5163320cf1
sofih-ii <sofiasaramoratrujillo@gmail.com>
2024-05-03 12:02:01 -0500

    Shows the sum of the even numbers from 1 to 100

---
+++ exercise_7.c
@@ -0,0 +1,14 @@
+//Shows the sum of the even numbers from 1 to 100
+/*
+#include <stdio.h>
+int main (void){
+  int x, sum;
+  sum = 0;
+  for(x=1; x<=100; x++){
+    if(x%2!= 0){
+      sum = sum +x;
+    }
+  }
+  printf("%d\n", sum);
+  return 0;
+}*/
